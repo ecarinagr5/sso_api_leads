faf6fa7af16326b08cd738cc535e83fd9b3d5244
Carina Gonzalez <carinagonzalez@MacBook-Pro-de-Carina.local>
2023-01-18 20:25:49 -0600

    vercel

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ app.use(bodyParser.json());
 
 // nuestra ruta irá en http://localhost:8080/api
 // es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
-const router = require("./routes");
+const router = require("./app/routes");
 app.use("/api", router);
 
 //arrancamos el servidor
 app.listen(port);
 console.log("API escuchando en el puerto " + port);
+
+// Export the Express API
+module.exports = app;
